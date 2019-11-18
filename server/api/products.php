f584ff295d5d4051c268b59a76ff6e41b5a74d4c
Kris <kmringler@gmail.com>
2019-11-18 21:20:16 +0000

    functionality works and am getting the example response back as shown in teh github example

--- server/api/products.php
+++ server/api/products.php
@@ -1,12 +1,25 @@
 <?php
 
 
+function getProducts(){
+
+$query =  "SELECT productId, name, price, image, shortDescription FROM `products`";
+$newLink = get_db_link();
+$queryResult = mysqli_query($newLink, $query);
+$fetch = mysqli_fetch_all($queryResult, MYSQLI_ASSOC);
+return $fetch;
+}
 
 $response['body'] = [
   "message" => "products go here"
 ];
 $response['status'] = 202;
 
-
-
-send($response);
+if ($request['method'] === 'GET') {
+  $link = get_db_link();
+  $message = getProducts($link);
+  $response['body'] = [
+    'message' => $message
+  ];
+  send($response);
+}
