61c71765474ea647d1a52453b66037c52578d240
Kris <kmringler@gmail.com>
2019-11-19 20:27:45 +0000

    exercise complete, sending 0 or an id that doesnt exist sends back an error, if you dont specify an id it gives the whole list, if you give it a valid id it gives you just the object of that one item

--- server/api/products.php
+++ server/api/products.php
@@ -1,14 +1,45 @@
 <?php
 
 
-function getProducts(){
+function getProducts()
+{
 
-$query =  "SELECT productId, name, price, image, shortDescription FROM `products`";
-$newLink = get_db_link();
-$queryResult = mysqli_query($newLink, $query);
-$fetch = mysqli_fetch_all($queryResult, MYSQLI_ASSOC);
-return $fetch;
+  $query =  "SELECT productId, name, price, image, shortDescription FROM `products`";
+  $newLink = get_db_link();
+  $queryResult = mysqli_query($newLink, $query);
+  $fetch = mysqli_fetch_all($queryResult, MYSQLI_ASSOC);
+  return $fetch;
 }
+
+
+
+if (isset($request['query']['productId'])) {
+  $productId = $request['query']['productId'] ?? null;
+  if (!intval($productId)) {
+    throw new ApiError('The product ID is not there', 400);
+  }
+  $query = "SELECT * FROM products WHERE productId = $productId";
+  $link = get_db_link();
+  $queryResult = mysqli_query($link, $query);
+  $fetch = mysqli_fetch_assoc($queryResult);
+  if ($fetch == null) {
+    throw new ApiError('The product ID is not there', 400);
+  }
+  $response['body'] = $fetch;
+  send($response);
+} else {
+  $query = "SELECT * FROM products";
+  $link = get_db_link();
+  $queryResult = mysqli_query($link, $query);
+  $fetch = mysqli_fetch_all($queryResult, MYSQLI_ASSOC);
+  if ($fetch == null) {
+    throw new ApiError('The product ID is not there', 400);
+  }
+  $response['body'] = $fetch;
+  send($response);
+}
+
+
 
 
 if ($request['method'] === 'GET') {
