0b9446167ffa9e5433a763208f9b2752924f90d8
Kris <kmringler@gmail.com>
2020-01-10 23:14:16 +0000

    added bootstrap classes to make it look cleaner and changed the form to have the correct inputs

--- client/components/cartSummaryItem.jsx
+++ client/components/cartSummaryItem.jsx
@@ -4,13 +4,14 @@ function CartSummaryItem(props) {
   const realPrice = `$${(props.product.price / 100).toFixed(2)}`;
 
   return (
-    <div className="d-flex justify-content mb-3 rounded">
+    <div className="d-flex justify-content mb-3 rounded container">
       <img className="photoSizer ml-5 m-3 rounded" src={props.product.image}></img>
       <div className="col-md-9 shadow card">
         <h3>{props.product.name}</h3>
         <p>{realPrice}</p>
-        <p>{props.product.shortDescription}</p>
-        <button onClick={product => props.deleteFromCart(props.product)} type="button" className="btn btn-danger">Remove From Cart</button>
+        <div >{props.product.shortDescription}
+          <button onClick={product => props.deleteFromCart(props.product)} type="button" className="btn btn-danger float-right">Remove From Cart</button>
+        </div>
       </div>
 
     </div>
--- client/components/checkoutForm.jsx
+++ client/components/checkoutForm.jsx
@@ -35,25 +35,25 @@ class CheckoutForm extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <form onSubmit={this.formSubmission}>
           <div className="form-row">
             <div className="form-group col-md-6">
-              <label>name</label>
-              <input type="name" className="form-control" id="inputEmail4" placeholder="name" name="name" onChange={this.updateName}></input>
+              <label>Name</label>
+              <input type="name" className="form-control" id="inputEmail4" placeholder="Name" name="name" onChange={this.updateName}></input>
             </div>
             <div className="form-group col-md-6">
-              <label htmlFor="inputPassword4">Password</label>
-              <input type="password" className="form-control" id="inputPassword4" placeholder="Password"></input>
+              <label htmlFor="inputPassword4">Email</label>
+              <input type="email" className="form-control" id="inputEmail4" placeholder="Email"></input>
             </div>
           </div>
           <div className="form-group">
             <label htmlFor="inputAddress">Credit Card</label>
-            <input type="text" className="form-control" id="inputAddress" name="creditCard" placeholder="1234 Main St" onChange={this.updateName}></input>
+            <input type="text" className="form-control" id="inputAddress" name="creditCard" placeholder="12345678" onChange={this.updateName}></input>
           </div>
           <div className="form-group">
             <label htmlFor="inputAddress2">Address </label>
-            <input type="text" className="form-control" id="inputAddress2" name="shippingAddress" placeholder="Apartment, studio, or floor" onChange={this.updateName}></input>
+            <input type="text" className="form-control" id="inputAddress2" name="shippingAddress" placeholder="1234 Main St" onChange={this.updateName}></input>
           </div>
           <div className="form-row">
             <div className="form-group col-md-6">
@@ -72,14 +72,7 @@ class CheckoutForm extends React.Component {
               <input type="text" className="form-control" id="inputZip"></input>
             </div>
           </div>
-          <div className="form-group">
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" id="gridCheck"></input>
-              <label className="form-check-label" htmlFor="gridCheck">
-                Check me out
-              </label>
-            </div>
-          </div>
+
           <button type="submit" className="btn btn-primary">Checkout</button>
         </form>
 
