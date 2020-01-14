9bca376f0699ac27000a3d8021d74c9684f0318d
Kris <kmringler@gmail.com>
2020-01-14 02:15:05 +0000

    changed products detail page to better design changed catalog list to have hover over features changed cartsummary layout

--- client/components/cartSummary.jsx
+++ client/components/cartSummary.jsx
@@ -6,34 +6,40 @@ function CartSummary(props) {
   const realPrice = `$${(totalPrice / 100).toFixed(2)}`;
   return (
     <div>
-      <div className="col-sm-12 d-flex justify-content-between ">
-        <div className='d-flex text-black mt-3 ml-5'
-          onClick={() => props.callback('catalog', {})}
-          style={{ cursor: 'pointer' }}>
-          <div className="fa fa-arrow-left py-2 fa-lg icon"></div>
-          <h4 className="ml-1">Back to Catalog</h4>
-        </div>
-        <div className="d-flex text-black">
-          <h2 className="mr-3 mt-2">My Cart</h2>
-        </div>
-        <div className="d-flex text-black mr-5">
-          <button className="btn btn-success m-2  d-flex " onClick={() => props.callback('checkout', {})}>Checkout</button>
-          <h2 className="ml-auto p-2">{realPrice}</h2>
-        </div>
-      </div>
+      <div className="container">
+        <div className="d-flex justify-content-end">
+          <div className=" text-black ">
+            <h2 className=" ">{realPrice}</h2>
+            <button className="btn btn-success  " onClick={() => props.callback('checkout', {})}>Checkout</button>
+          </div>
 
-      <div className="container ">
+        </div>
+        <div className="col-sm-12 d-flex justify-content-between ">
+          <div className='d-flex text-black mt-3 ml-5'
+            onClick={() => props.callback('catalog', {})}
+            style={{ cursor: 'pointer' }}>
+            <div className="fa fa-arrow-left py-2 fa-lg icon"></div>
+            <h4 className="ml-1">Back to Catalog</h4>
+          </div>
 
-        {props.cartInfo.map((product, index) => (
-          <CartSummaryItem
-            key={index}
-            product={product}
-            callback={props.callback}
-            deleteFromCart={product => props.removeCallback(product)}
-          />
-        ))}
-      </div>
-    </div>
+        </div>
+
+        <div className="container ">
+
+          {props.cartInfo.map((product, index) => (
+            <CartSummaryItem
+              key={index}
+              product={product}
+              callback={props.callback}
+              deleteFromCart={product => props.removeCallback(product)}
+
+            />
+
+          ))}
+
+        </div>
+      </div >
+    </div >
   );
 }
 
