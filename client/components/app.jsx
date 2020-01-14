08b8d3346f35839c3d173d988a4b1e0119816589
Kris <kmringler@gmail.com>
2020-01-14 21:55:13 +0000

    changed pathing for fetch requests

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -32,7 +32,7 @@ export default class App extends React.Component {
   }
 
   getCartItems() {
-    fetch('/api/cart')
+    fetch('server/api/cart.php')
       .then(cartData => cartData.json())
       .then(jsonData => {
 
@@ -46,7 +46,7 @@ export default class App extends React.Component {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(product)
     };
-    fetch('api/cart', required)
+    fetch('server/api/cart.php', required)
       .then(productData => productData.json())
       .then(jsonProductData => {
         this.setState({ cart: this.state.cart.concat(jsonProductData) });
@@ -60,7 +60,7 @@ export default class App extends React.Component {
       body: JSON.stringify(product)
 
     };
-    fetch('api/cart', required)
+    fetch('server/api/cart.php', required)
       .then(productData => productData.json())
       .then(jsonProductData => {
         const array = [...this.state.cart];
