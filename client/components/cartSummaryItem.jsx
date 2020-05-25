78c336f69b2aae62f90d4e3ffbaa003a6a7fee17
Kris <kmringler@gmail.com>
2020-05-25 06:30:19 +0000

    card styling reformatted to flex, colors better to match the rest of the site rounded edges buttons are red

--- client/components/cartSummaryItem.jsx
+++ client/components/cartSummaryItem.jsx
@@ -4,69 +4,70 @@ function CartSummaryItem(props) {
   const realPrice = `$${(props.product.price / 100).toFixed(2)}`;
   return (
 
-    < div >
-      <div className="container mb-3">
-        <div className="row border bg-white rounded p-3 position-relative item-card ">
-          <div className="d-flex justify-content-between w-100">
-            <h1 className="d-flex ">{props.product.name}</h1>
-            <i className="far fa-times-circle cancel-button pointer" onClick={() => {
-              props.productToRemove(props.product);
-              props.toggleModal();
-            }} />
+    <div className="container mb-3 item-card bg-white imageRounder">
 
-          </div>
-          <div className="col-md-4 m-auto ">
-            <img src={props.product.image} className="fittingObj "></img>
+      <div className=" d-flex  ">
 
-          </div>
-          <div className="col-md-8 m-auto ">
+        <h1 className="mr-auto p-2">{props.product.name}</h1>
 
-            <h2 className="font-weight-bold col-md-6 productPrice">{realPrice}</h2>
-            <p className="col-md-12 ">{props.product.longDescription}</p>
-            <div className="d-flex ">
-              <h5 className=" col-md-1 m-auto flex-row-reverse ">Quantity: </h5>
-              <div className="d-flex btn-group mx-3 my-auto border border-dark rounded ">
-
-                <div className="d-flex">
-
-                  <button className="btn btn-light rounded-light "
-                    onClick={() => {
-                      if (props.product.quantity > 1) {
-                        props.addToCart(props.product, '-');
-                      } else {
-                        props.productToRemove(props.product);
-                        props.toggleModal();
-                      }
-                    }}>
-                    <i className="fas fa-minus m-auto" />
-                  </button>
-                </div>
-                <div className="d-flex p-3">
-                  <h5 className="m-auto">{props.product.quantity}</h5>
-                </div>
-                <div className="d-flex">
-                  <button className="btn btn-light rounded-left"
-                    onClick={() => {
-
-                      props.addToCart(props.product, '+');
-
-                    }}>
-                    <i className="fas fa-plus m-auto" />
-
-                  </button>
-                </div>
-
-              </div>
-
-            </div>
-
-          </div>
-
-        </div >
+        <i className="far fa-times-circle cancel-button pointer p-2" onClick={() => {
+          props.productToRemove(props.product);
+          props.toggleModal();
+        }} />
 
       </div>
 
+      <div className="d-flex">
+        <img src={props.product.image} className="fittingObj mr-auto p-2" />
+        <div className="p-2">
+
+          <div className="d-flex justify-content-end">
+
+            <h2 className="  productPrice">{realPrice}</h2>
+
+          </div>
+
+          <div>
+            <p >{props.product.longDescription}</p>
+          </div>
+        </div>
+
+      </div>
+      <div className="d-flex justify-content-end">
+        <div className="quantity">Quantity: </div>
+        <div className=" btn-group mx-3 my-auto border border-dark rounded ">
+          <div className="d-flex">
+            <button className="btn btn-light rounded-light "
+              onClick={() => {
+                if (props.product.quantity > 1) {
+                  props.addToCart(props.product, '-');
+                } else {
+                  props.productToRemove(props.product);
+                  props.toggleModal();
+                }
+              }}>
+              <i className="fas fa-minus m-auto" />
+            </button>
+          </div>
+
+          <div className="d-flex p-3">
+            <h5 className="m-auto">{props.product.quantity}</h5>
+          </div>
+
+          <div className="d-flex">
+            <button className="btn btn-light rounded-left"
+              onClick={() => {
+                props.addToCart(props.product, '+');
+              }}>
+              <i className="fas fa-plus m-auto" />
+
+            </button>
+          </div>
+        </div>
+      </div>
+
     </div>
+
   );
 }
 
